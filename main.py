00dfee462bd359c69ef275323998b01dc5d9314e
Manuel <rochmanuel97@gmail.com>
2025-11-12 23:40:15 +0500

    Initial commit

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-def add(a, b):
-    return a + b
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"message": "Приложение успешно развернуто на Vercel!"}
+
+@app.get("/about")
+def about():
+    return {"info": "Это тестовый API на FastAPI"}
